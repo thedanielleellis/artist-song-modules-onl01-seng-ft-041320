2055acce83f18cc2bc9fdc18677e573df81da3fa
Danielle Ellis <thedanielleellis@gmail.com>
2020-05-01 06:06:36 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,13 @@
 module Findable 
   module ClassMethods
     
-  def find_by_name
-  end
   
     def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
+  end
+  
+    def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
   end
   
 end 
